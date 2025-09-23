ebd4b1db9a4f9a9acb2b5851ddc7f0d1f256a4e9
Marcel24-Ariyanto <marcel24ti@mahasiswa.pcr.ac.id>
2025-09-23 10:50:19 +0700

    Latihan Passing Data dan Git

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+     public function index()
+    {
+        // Tanggal wisuda: 15 Oktober 2025
+        $tanggal_wisuda = Carbon::createFromDate(2027, 10, 15);
+
+        // Tanggal lahir (tahun saja)
+        $tanggalLahir = Carbon::createFromFormat('Y', '2006');
+
+        // Tanggal sekarang
+        $tanggalSekarang = Carbon::now();
+
+        // Data lainnya
+        $data['username'] = 'Marcel Ariyanto';
+
+        // Menghitung umur dalam tahun penuh tanpa koma
+        $data['umur'] = (int) $tanggalLahir->diffInYears($tanggalSekarang);
+
+
+        $data['current_semester']=3;
+
+        // Hobi
+        $data['hobbi'] = ['Badminton', 'Game', 'Berenang', 'Mancing', 'Catur', 'Jalan-jalan'];
+
+        // Menghitung selisih hari antara tanggal sekarang dan tanggal wisuda
+        $data['time_to_study_left'] = (int) $tanggalSekarang->diffInDays($tanggal_wisuda);
+
+        $data['future_goal']="Bahagia";
+        // Mengembalikan data ke view
+        return view('home', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+
+// pertemuan 2
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatkulController;
+use App\Http\Controllers\Carbon;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+});
+// parameter reguired
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+//  parameter anti required
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswass';
+})->name('mahasiswa.show');
+
+
+
+Route::get('/mahasiswa/{param1}',[MahasiswaController::class, 'show']);
+
+
+
+
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+
+
+
+Route::get('/matakuliah/{param1?}',[MatkulController::class,'show']);
+
+
+
+
+
+
+
+//pertemuan 3
+Route::get('/home',[HomeController::class,'index']);
+
